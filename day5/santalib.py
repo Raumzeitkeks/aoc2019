b2c0d3f4abb7f7cc2bff1fe4874be60bc2e0f61c
Giulia Ruocco <giulia.ruocco@mailbox.org>
2019-12-06 21:41:57 +0100

    Day 5b

--- day5/santalib.py
+++ day5/santalib.py
@@ -1,6 +1,6 @@
 
 class IntCode:
-    class HLT(Exception):
+    class Halt(Exception):
         pass
     
     class HCF(Exception):
@@ -22,7 +22,7 @@ class IntCode:
         try:
             while True:
                 self.step()
-        except self.HLT:
+        except self.Halt:
             pass
     
     def step(self):
@@ -33,76 +33,70 @@ class IntCode:
         op(*params)
     
     def _decode_xop(self, xop):
-        add = lambda v1, v2, d: self._op_add(v1, v2, d)
-        mul = lambda v1, v2, d: self._op_mul(v1, v2, d)
-        get = lambda d: self._op_get(d)
-        put = lambda v: self._op_put(v)
-        jit = lambda v, ip: self._op_jit(v, ip)
-        jif = lambda v, ip: self._op_jif(v, ip)
-        les = lambda v1, v2, d: self._op_les(v1, v2, d)
-        equ = lambda v1, v2, d: self._op_equ(v1, v2, d)
-        hlt = lambda : self._op_hlt()
-        hcf = lambda : self._op_hcf("Invalid xop [{}]".format(xop))
-        P, I, W, J = "PIWJ"
-        lut = {   1: (add, (P, P, W)),
-                101: (add, (I, P, W)),
-               1001: (add, (P, I, W)),
-               1101: (add, (I, I, W)),
-                  2: (mul, (P, P, W)),
-                102: (mul, (I, P, W)),
-               1002: (mul, (P, I, W)),
-               1102: (mul, (I, I, W)),
-                  3: (get, (W)),
-                  4: (put, (P)),
-                104: (put, (I)),
-                  5: (jit, (P, J)),
-                105: (jit, (I, J)),
-                  6: (jif, (P, J)),
-                106: (jif, (I, J)),
-                  7: (les, (P, P, W)),
-                107: (les, (I, P, W)),
-               1007: (les, (P, I, W)),
-               1107: (les, (I, I, W)),
-                  8: (equ, (P, P, W)),
-                108: (equ, (I, P, W)),
-               1008: (equ, (P, I, W)),
-               1108: (equ, (I, I, W)),
-                 99: (hlt, ())}
-        return lut.get(xop, (hcf, ()))
+        P, I, W = "PIW"
+        lut = {   1: (self._add,  (P, P, W)),
+                101: (self._add,  (I, P, W)),
+               1001: (self._add,  (P, I, W)),
+               1101: (self._add,  (I, I, W)),
+                  2: (self._mul,  (P, P, W)),
+                102: (self._mul,  (I, P, W)),
+               1002: (self._mul,  (P, I, W)),
+               1102: (self._mul,  (I, I, W)),
+                  3: (self._get,  (W)      ),
+                  4: (self._put,  (P)      ),
+                104: (self._put,  (I)      ),
+                  5: (self._jnz,  (P, P)   ),
+                105: (self._jnz,  (I, P)   ),
+               1005: (self._jnz,  (P, I)   ),
+               1105: (self._jnz,  (I, I)   ),
+                  6: (self._jz,   (P, P)   ),
+                106: (self._jz,   (I, P)   ),
+               1006: (self._jz,   (P, I)   ),
+               1106: (self._jz,   (I, I)   ),
+                  7: (self._lt,   (P, P, W)),
+                107: (self._lt,   (I, P, W)),
+               1007: (self._lt,   (P, I, W)),
+               1107: (self._lt,   (I, I, W)),
+                  8: (self._eq,   (P, P, W)),
+                108: (self._eq,   (I, P, W)),
+               1008: (self._eq,   (P, I, W)),
+               1108: (self._eq,   (I, I, W)),
+                 99: (self._halt, ()       )}
+        return lut.get(xop, (lambda: self._hcf(xop), ()))
         
-    def _op_add(self, val1, val2, dest):
+    def _add(self, val1, val2, dest):
         self._mem[dest] = val1 + val2
         self._ip += 4
         
-    def _op_mul(self, val1, val2, dest):
+    def _mul(self, val1, val2, dest):
         self._mem[dest] = val1 * val2
         self._ip += 4
         
-    def _op_get(self, dest):
+    def _get(self, dest):
         self._mem[dest] = next(self._in)
         self._ip += 2
         
-    def _op_put(self, val):
+    def _put(self, val):
         self._out(val)
         self._ip += 2
         
-    def _op_jit(self, val, ip):
-        self._ip = ip if val else self._ip + 3
+    def _jnz(self, val, addr):
+        self._ip = addr if val != 0 else self._ip + 3
             
-    def _op_jif(self, val, ip):
-        self._ip = ip if not val else self._ip + 3
+    def _jz(self, val, addr):
+        self._ip = addr if val == 0 else self._ip + 3
         
-    def _op_les(self, val1, val2, dest):
+    def _lt(self, val1, val2, dest):
         self._mem[dest] = 1 if val1 < val2 else 0
         self._ip += 4
         
-    def _op_equ(self, val1, val2, dest):
+    def _eq(self, val1, val2, dest):
         self._mem[dest] = 1 if val1 == val2 else 0
         self._ip += 4
         
-    def _op_hlt(self, *args):
-        raise self.HLT(*args)
+    def _halt(self, *args):
+        raise self.Halt(*args)
         
-    def _op_hcf(self, *args):
+    def _hcf(self, *args):
         raise self.HCF(*args)
         
--- day5/test_aoc.py
+++ day5/test_aoc.py
@@ -1,15 +1,29 @@
 import santalib as santa 
 
-def no_output():
-    assert False, "Unexpected output"
-
-def test_aoc_examples():
+def test_aoc_examples_dump():
     examples = [
-        ([1002,4,3,4,33], [1002,4,3,4,99], [], no_output),
-        ([1101,100,-1,4,0], [1101,100,-1,4,99], [], no_output)
+        ([1002,4,3,4,33], [1002,4,3,4,99]),
+        ([1101,100,-1,4,0], [1101,100,-1,4,99])
         ]
-    for before, after, input, output in examples:
-        ic = santa.IntCode(before, input, output)
+    for before, after in examples:
+        ic = santa.IntCode(before, [], lambda _: None)
         ic.run()
         assert ic.dump() == after
         
+def test_aoc_examples_output():
+    examples = [
+        ([3,9,8,9,10,9,4,9,99,-1,8], [(-8,0), (7,0), (8,1), (9,0), (10,0)]),
+        ([3,9,7,9,10,9,4,9,99,-1,8], [(-8,1), (7,1), (8,0), (9,0), (10,0)]),
+        ([3,3,1108,-1,8,3,4,3,99],   [(-8,0), (7,0), (8,1), (9,0), (10,0)]),
+        ([3,3,1107,-1,8,3,4,3,99],   [(-8,1), (7,1), (8,0), (9,0), (10,0)]),
+        ([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9], [(-5,1), (-1,1), (0,0), (1,1), (5,1)]),
+        ([3,3,1105,-1,9,1101,0,0,12,4,12,99,1],      [(-5,1), (-1,1), (0,0), (1,1), (5,1)]),
+        ([3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99], [(-8,999), (7,999), (8,1000), (9,1001), (10,1001)]),
+        ]
+    for code, in_out in examples:
+        for input, output in in_out:
+            print(code, input, output)
+            out = []
+            ic = santa.IntCode(code, [input], out.append)
+            ic.run()
+            assert [output] == out
